d3bb6d12c9731ce0a7489c79e7c0ccf88ab36a4f
go57var <go57var@mytum.de>
2026-01-11 18:46:37 +0100

    2026.01.08 update running man

---
+++ assets/texture/basics.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="basics" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="basics.png" width="303" height="132"/>
+</tileset>
